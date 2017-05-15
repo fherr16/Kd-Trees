a0f855f08ea7aca0ff77f906973533156ce2cebf
fabianherrera <fabianherrera@192.168.1.8>
2017-05-14 22:26:12 -0700

    Untested Neighbor method

--- src/KdTree.java
+++ src/KdTree.java
@@ -125,7 +125,7 @@ public class KdTree {
   }
   
   public void draw() {
-    
+    inOrder(root);
   }
   
   public Iterable<Point2D> range(RectHV rect) {
@@ -133,7 +133,81 @@ public class KdTree {
   }
   
   public Point2D nearest(Point2D p) {
-    return null;
+    if (root == null)
+      throw new NullPointerException();
+    
+    Point2D closest = root.value;
+    Point2D leftSubTree, rightSubTree;
+    
+    if (p.x() < root.value.x()) {
+      leftSubTree = checkLeft(root.left, closest, p);
+      if (p.distanceTo(leftSubTree) < (root.value.x() - p.x())) return leftSubTree;
+      rightSubTree = checkRight(root.right, closest, p);
+    }
+    else {
+      rightSubTree = checkRight(root.right, closest, p);
+      if (p.distanceTo(rightSubTree) < (p.x() - root.value.x())) return rightSubTree;
+      leftSubTree = checkLeft(root.left, closest, p);
+    }
+        
+    if (p.distanceTo(leftSubTree) < p.distanceTo(closest)) closest = leftSubTree;
+    if (p.distanceTo(rightSubTree) < p.distanceTo(closest)) closest = rightSubTree;
+    
+    return closest;
+  }
+  
+  private Point2D checkLeft(Node left, Point2D closest, Point2D p) {
+    if (left != null) {
+      if (p.distanceTo(left.value) < p.distanceTo(closest) && p.distanceTo(left.value) != 0.0) closest = left.value;
+      
+      Point2D leftSubTree, rightSubTree;
+      
+      if (left.compareX && p.x() < left.value.x()) {
+        leftSubTree = checkLeft(left.left, closest, p);
+        if (p.distanceTo(leftSubTree) < (left.value.x() - p.x())) return leftSubTree;
+        rightSubTree = checkRight(left.right, closest, p);
+      }
+      else if (left.compareX && p.x() > left.value.x()){
+        rightSubTree = checkRight(left.right, closest, p);
+        if (p.distanceTo(rightSubTree) < (p.x() - left.value.x())) return rightSubTree;
+        leftSubTree = checkLeft(left.left, closest, p);
+      }
+      else if (!left.compareX && p.y() < left.value.y()) {
+        leftSubTree = checkLeft(left.left, closest, p);
+        if (p.distanceTo(leftSubTree) < (left.value.y() - p.y())) return leftSubTree;
+        rightSubTree = checkRight(left.right, closest, p);
+      }
+      else {
+        rightSubTree = checkRight(left.right, closest, p);
+        if (p.distanceTo(rightSubTree) < (p.y() - left.value.y())) return rightSubTree;
+        leftSubTree = checkLeft(left.left, closest, p);
+      }
+      
+      if (p.distanceTo(leftSubTree) < p.distanceTo(closest)) closest = leftSubTree;
+      if (p.distanceTo(rightSubTree) < p.distanceTo(closest)) closest = rightSubTree;
+    }
+    return closest;
+  }
+  
+  private Point2D checkRight(Node right, Point2D closest, Point2D p) {
+    if (right != null) {
+      if (p.distanceTo(right.value) < p.distanceTo(closest) && p.distanceTo(right.value) != 0.0) closest = right.value;
+      
+      Point2D leftSubTree = checkLeft(right.left, root.value, p);
+      Point2D rightSubTree = checkRight(right.right, root.value, p);
+      
+      if (p.distanceTo(leftSubTree) < p.distanceTo(closest)) closest = leftSubTree;
+      if (p.distanceTo(rightSubTree) < p.distanceTo(closest)) closest = rightSubTree;
+    }
+    return closest;
+  }
+  
+  private void inOrder(Node current) {
+    if (current != null) {
+      inOrder(current.left);
+      current.value.draw();
+      inOrder(current.right);
+    }
   }
 
 }
